9dea8833e4ca531e9373b683fc60baae9dc819a9
Alex Njoroge <alexnjoroge919@gmail.com>
2026-06-03 11:52:15 +0300

    feat: integrate Google Analytics 4 in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import App from './App.tsx';
 import './index.css';
+
+ReactGA.initialize('G-CEZEG72V6E');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
